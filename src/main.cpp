a60e9439dcf6b4acc5d7fe31e56763f24fd99123
Msi <Msi@DESKTOP-LVCDSAI>
2025-08-24 19:47:55 +0300

    day1 done read and tokeniz

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,53 @@
 #include<iostream>
 #include<string>
+#include<sstream>
+#include<fstream>
+#include<vector>
 using namespace std;
 
-int main(){
+
+// takes file convert it to string and return it
+string readfile(const string& filename){
     
+    ifstream file(filename);
+
+    if(file.fail()){
+        cout<<"error to open "<<filename<<endl;
+        return "";
+    }
+    // stringstream buffer;
+    // buffer<<file.rdbuf();
+    // return buffer.str();
+    string line , content;
+    while(getline(file,line)){
+        content = content+line+"\n";
+
+    }
+    return content;
+
+}
+
+
+// takes string split it on any space or , then return it in a vector
+vector<string> tokenization(const string& text){
+    vector<string> tokens;
+    stringstream ss(text);
+    string word;
+    while(ss>>word){
+        tokens.push_back(word);
+    }
+    return tokens;
+}
+
+
+int main(){
+// string filenam;
+// cout<<"enter : ";
+// getline(cin,filenam);
+string content = readfile("../docs/doc1.txt");
+vector<string> words=tokenization(content);
+ for(const string& word : words){
+    cout<<word<<endl;
+ }
+
 }
